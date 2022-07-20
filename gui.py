4f22960aec3272efc582cdcc618558d5846a9afb
Kevin Leroux <k.lrx076@gmail.com>
2022-07-20 19:38:03 +0200

    button methods

--- gui.py
+++ gui.py
@@ -20,4 +20,27 @@ class CurrencyConverter:
         
         
         pass
- 
+    def invert(self) -> None:
+        self.from_currency = self.from_currency_entry.get()
+        self.to_currency = self.to_currency_entry.get()
+        self.from_currency_entry.delete(0, tk.END)
+        self.from_currency_entry.insert(0, self.to_currency)
+        self.to_currency_entry.delete(0, tk.END)
+        self.to_currency_entry.insert(0, self.from_currency)
+        pass
+    def close_window(self) -> None:
+        self.root.destroy()
+        pass
+    def convert(self) -> None:
+        try:
+            self.amount = float(self.amount_entry.get())
+            self.from_currency = self.from_currency_entry.get()
+            self.to_currency = self.to_currency_entry.get()
+            self.result = self.amount * float(CurrencyRates().get_rate(self.from_currency, self.to_currency))
+            # get to_currency symbol
+            self.to_currency_symbol = CurrencyCodes().get_symbol(self.to_currency)
+            self.result_label.configure(text=str(self.result) + " " + str(self.to_currency_symbol))
+        except ValueError:
+            self.result_label.configure(text="Invalid input")
+        pass
+
